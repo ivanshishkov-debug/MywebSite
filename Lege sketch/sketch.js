f10c4264173da1775b334d6f603d04e69ac596df
ivanshishkov-debug <ivanshishkov.ua@gmail.com>
2025-09-09 15:59:23 +0200

    dag 1
    xqq

---
+++ Lege sketch/sketch.js
@@ -0,0 +1,59 @@
+function setup() {
+  createCanvas(850, 400);
+}
+
+function draw() {
+  background(200);
+  fill("red");
+  rect(50,60,100,20)
+  
+  fill("white");
+  rect(50,80,100,20)
+    fill("blue");
+rect(50,100,100,20)
+fill("purple")
+text(" 1. Ivan Shyshkov", 50,50);
+fill("black")
+rect(50,150,20,20)
+fill("black")
+rect(50,190,20,20)
+fill("white")
+rect(50,170,20,20)
+fill("white")
+rect(70,150,20,20)
+fill("black")
+rect(90,150,20,20)
+fill("black")
+rect(90,190,20,20)
+fill("white")
+rect(70,190,20,20)
+fill("white")
+rect(90,170,20,20)
+fill("black")
+rect(70,170,20,20)
+
+fill(0, 0)
+triangle(50,290,105,290,77,260)
+fill(0, 0)
+rect(51,290,53,53)
+
+
+
+fill("grey")
+rect(230,60,50,120)
+
+fill("red")
+circle(255,80,30)
+fill("orange")
+circle(255,120,30)
+fill("green")
+circle(255,160,30)
+fill("grey")
+rect(245,180,20,50)
+
+
+
+
+
+
+}
