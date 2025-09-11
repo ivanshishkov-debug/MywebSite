8e222e2bd4b0074278ee87a2c9fb859f553c43e1
ivanshishkov-debug <ivanshishkov.ua@gmail.com>
2025-09-11 10:20:29 +0200

    week 1 af

--- Lege sketch/sketch.js
+++ Lege sketch/sketch.js
@@ -1,8 +1,16 @@
+let img; 
+function preload() { 
+  img=loadImage("assets/images.jfif");
+}
+
+
 function setup() {
   createCanvas(850, 400);
+  img.resize(120,0)
 }
 
 function draw() {
+  strokeWeight(1)
   background(200);
   fill("red");
   rect(50,60,100,20)
@@ -53,6 +61,122 @@ rect(245,180,20,50)
 
 
 
+fill("white")
+strokeWeight(4)
+square(350,100,120,10);
+
+
+strokeWeight(1)
+fill("black")
+circle(375,125,32)
+ 
+fill("black")
+circle(407,165,32)
+
+
+fill("black")
+circle(443,199,32)
+
+
+
+
+fill("red")
+rect(600,100,10,10)
+fill("red")
+rect(610,100,10,10)
+fill("red")
+rect(620,100,10,10)
+fill("red")
+rect(630,100,10,10)
+fill("red")
+rect(640,100,10,10)
+fill("red")
+rect(650,100,10,10)
+fill("red")
+rect(660,100,10,10)
+fill("red")
+rect(590,110,10,10)
+fill("red")
+rect(600,110,10,10)
+fill("red")
+rect(610,110,10,10)
+fill("red")
+rect(620,110,10,10)
+fill("red")
+rect(630,110,10,10)
+fill("red")
+rect(640,110,10,10)
+fill("red")
+rect(650,110,10,10)
+fill("red")
+rect(660,110,10,10)
+fill("red")
+rect(670,110,10,10)
+fill("red")
+rect(680,110,10,10)
+fill("red")
+rect(690,110,10,10)
+fill("#FFC89F")
+rect(620,120,20,50)
+rect(600,170,80,10)
+rect(600,160,20,10)
+rect(610,140,10,10)
+rect(590,140,10,20)
+rect(640,150,10,10)
+rect(650,140,30,10)
+rect(660,150,30,10)
+rect(670,210,20,30)
+rect(660,220,10,10)
+rect(570,210,20,30)
+rect(590,220,10,10)
+fill("red")
+rect(590,180,20,30)
+rect(590,210,10,10)
+rect(570,200,20,10)
+rect(580,190,10,10)
+rect(620,180,20,20)
+rect(620,180,30,10)
+rect(640,180,10,30)
+rect(650,180,10,30)
+rect(660,190,20,20)
+rect(660,210,10,10)
+rect(680,200,10,10)
+
+
+
+
+
+
+fill("blue")
+rect(600,210,60,30)
+rect(590,230,30,20)
+rect(640,230,30,20)
+rect(610,180,10,30)
+rect(610,200,40,10)
+rect(640,190,10,10)
+
+
+fill("brown")
+rect(610,150,10,10)
+rect(590,120,30,20)
+rect(600,140,10,20)
+rect(580,160,20,10)
+rect(580,140,10,20)
+rect(650,250,30,20)
+rect(680,260,10,10)
+rect(580,250,30,20)
+rect(570,260,10,10)
+fill("yellow")
+rect(640,210,10,10)
+rect(610,210,10,10)
+fill("black")
+rect(640,160,40,10)
+rect(650,150,10,10)
+rect(640,130,10,20)
+
+
+
+image(img,300,220)
 
 
 
