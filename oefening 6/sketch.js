2891999bec2623725f8e9f745d4bf8b90205c421
ivanshishkov-debug <ivanshishkov.ua@gmail.com>
2025-10-03 10:48:41 +0200

    week 4

---
+++ oefening 6/sketch.js
@@ -0,0 +1,21 @@
+
+function setup() {
+  createCanvas(800, 400);
+}
+
+function keyPressed() {
+ 
+
+}
+function draw() {
+background(220)
+
+for(let i =0; i < 10;i++){
+rect(15,15,50,50)
+
+
+
+}
+
+
+}
--- week 4/sketch.js
+++ week 4/sketch.js
@@ -1,13 +1,77 @@
-
+let vierhoek = [];
+let circles = [];
 function setup() {
-  createCanvas(1000, 800);
+  createCanvas(800, 600);
 
 
 
 }
 function draw() {
- 
+ background(0,20)
+for (let c of circles ){////рисоавка кругов в дроу
+  fill(random(255),random(255),random(255));
+  noStroke();
+ellipse(c.x,c.y,c.r);
+c.x+=c.dx;
+c.y +=c.dy;
+}
+for (let v of vierhoek){////рисоавка квадратов в дроу
+fill(random(255),random(255),random(255));
+  noStroke();
+  rect(v.x,v.y,v.w,v.h);
+  v.x+=v.dx;
+  v.y+=v.dy;
 
+
+}
+
+
+}
+function keyPressed(){
+if (keyCode === BACKSPACE) {
+  circles = [];
+  vierhoek = [];
+for  ( let i = 0;i < 20;i++){///основной спавн и цвет кругов
+let x=random(width);
+let y= random(height);
+let r= random(20,100);
+fill(random(255),random(255),random(255));
+noStroke();
+let circle = {
+
+
+
+
+  x:x,
+  y:y,
+  r:r,
+  dx : random (-2,2),///скорость по X
+  dy : random (-2,2)///скорость по Y
+
+};
+circles.push(circle);
+
+}
+ for (let i=0;i < 20; i ++){///основной спавн и цвет квадратов
+let x=random(width);
+let y= random(height);
+let w= random(20,100);
+let h= random(20,100);
+fill(random(255),random(255),random(255));
+noStroke();
+let square = {
+x:x,
+y:y,
+w:w,
+h:h,
+dx : random (-2,2),
+  dy : random (-2,2)
+
+};
+vierhoek.push(square);
+ } 
+
+}
 
 
 
